0d094ef8ced1b07ccbc72786fbb2e8f656cfa8ad
Alymov Roman <romanalym@gmail.com>
2019-08-19 17:19:43 +0400

    feat(app): vue-cli 3 integration

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang=en>
+    <head>
+        <meta charset=utf-8>
+        <meta http-equiv=X-UA-Compatible content="IE=edge">
+        <meta name=viewport content="width=device-width,initial-scale=1">
+        <link rel=icon href=/favicon.ico>
+        <title>frontend</title>
+        <link href=/js/about.7998d812.js rel=prefetch>
+        <link href=/css/app.7ca4fd17.css rel=preload as=style>
+        <link href=/js/app.ed0f7949.js rel=preload as=script>
+        <link href=/js/chunk-vendors.e9885347.js rel=preload as=script>
+        <link href=/css/app.7ca4fd17.css rel=stylesheet>
+    </head>
+    <body>
+        <noscript><strong>We're sorry but frontend doesn't work properly without JavaScript enabled. Please enable it to
+                continue.</strong></noscript>
+        <div id=app></div>
+        <script src=/js/chunk-vendors.e9885347.js></script>
+        <script src=/js/app.ed0f7949.js></script>
+    </body>
+</html>
